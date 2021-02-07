492d6ad347217971cf484c7133f6f8c7e7df9e55
Huy Anh <huyanh151998@gmail.com>
2021-02-07 11:30:19 +0700

    .

---
+++ js/home.js
@@ -0,0 +1,24 @@
+$('.feature__slider1').slick({
+    dots: false,
+    arrows: true,
+    prevArrow: '.feature__arrow-prev1',
+    nextArrow: '.feature__arrow-next1',
+})
+$('.feature__slider2').slick({
+    dots: false,
+    arrows: true,
+    prevArrow: '.feature__arrow-prev2',
+    nextArrow: '.feature__arrow-next2',
+})
+$('.feature__slider3').slick({
+    dots: false,
+    arrows: true,
+    prevArrow: '.feature__arrow-prev3',
+    nextArrow: '.feature__arrow-next3',
+})
+$('.feature__slider4').slick({
+    dots: false,
+    arrows: true,
+    prevArrow: '.feature__arrow-prev4',
+    nextArrow: '.feature__arrow-next4',
+})
